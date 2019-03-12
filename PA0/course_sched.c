28b9edcd2ccbc0cd1ac96d8db3467de542083f0a
peterhyun1234 <peterhyun1234@gmail.com>
2019-03-12 13:40:56 +0900

    After parsing

--- PA0/course_sched.c
+++ PA0/course_sched.c
@@ -15,6 +15,7 @@ struct CourseEntry {
   float difficulty; // 과목의 난이도
 };
 
+void rmvfirst(char* buf);
 int main(int argc, char** argv)
 {
 
@@ -27,6 +28,10 @@ int main(int argc, char** argv)
 	int N = 0;
 	char* info[10];
 	char buf[BUFSIZ];
+	char sub1[BUFSIZ];
+	char sub2[BUFSIZ];
+	memset(sub1, 0x00, BUFSIZ);
+	memset(sub2, 0x00, BUFSIZ);
 	memset(buf, 0x00, BUFSIZ);
 	fp = fopen("./in/database.csv", "r");
 
@@ -36,7 +41,7 @@ int main(int argc, char** argv)
 		while(!feof(fp))
 		{
 
-			fgets(buf, 100, fp);
+			fgets(buf, 1000, fp);
 			char* newstrptr = (char*)malloc(sizeof(char)*(strlen(buf)+1));
 			strcpy(newstrptr,buf);
 			info[n] = newstrptr;
@@ -53,6 +58,8 @@ int main(int argc, char** argv)
 
 		strcpy(s1, info[i]);
 
+		printf("[%d] %s\n",i,info[i]);
+
 		char* ptr = strtok(s1, ",");
 		while(ptr != NULL)
 		{
@@ -62,31 +69,36 @@ int main(int argc, char** argv)
 			}
 			else if(N == 1)	// 선수 과목의 수 
 			{
-				ce[i].prerequisites = atoi(ptr);
+				ce[i].n_prerequisites = atoi(ptr);
+//				ce[i].prerequisites = malloc(sizeof(struct CourseEntry)*ce[i].n_prerequisites);
 			}
 			else if(N == 2)	// 선수 과목 1
 			{
-				strcpy(ce[i].prerequisites[0].name, ptr);
+				rmvfirst(ptr);
+	 			strcpy(sub1, ptr);
 			}
 			else	// 선수 과목 2
-			{
-				strcpy(ce[i].prerequisites[1].name, ptr);
+			{	
+				rmvfirst(ptr);
+				strcpy(sub2, ptr);
 			}
 
-
-			ptr = strtok(NULL, " ,");
+			ptr = strtok(NULL, ",");
+		 //	printf("ptr[%d] %s\n",i, ptr); 
 			N++;
 		}
+			printf("sub1 =%s\nsub2 =%s\n", sub1, sub2);
 		N = 0;
 	}
 	
 
 
-
+/*
 
 for(int a = 0; a < i; a++)
 	{
 	
+
 		printf("과목이름 : %s\n", ce[a].name);
 		printf("과목 난이도 : %f\n", ce[a].difficulty);
 		printf("선수 과목의 수 : %d\n", ce[a].n_prerequisites);
@@ -96,8 +108,19 @@ for(int a = 0; a < i; a++)
 
 		printf("\n\n");
 	}
-
+*/
 	fclose(fp);
 
   return 0;
+}
+
+
+
+void rmvfirst(char *buf)
+{
+	int i=0;
+	for(i=1;buf[i];i++)
+		buf[i-1] = buf[i];
+
+	buf[i-1] = '\0';
 }
